e7ea8f24f34e379db24fdb3183c76a45adfc660e
alexkaasik <alex.kaasik@outlook.com>
2024-08-26 16:42:07 +0300

    Added even more CLASSES

---
+++ ConsoleUM/Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+
+namespace ConsoleUM.Data
+{
+    public class DbInitializer
+    {
+        public static void Intialize(SchoolContext context) {
+            context.Database.EnsureCreated();
+            if (context.Student.Any())
+            {
+                return;
+            }
+        }
+    }
+}
--- ConsoleUM/Program.cs
+++ ConsoleUM/Program.cs
@@ -1,3 +1,6 @@
+using ConsoleUM.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ConsoleUM
 {
     public class Program
@@ -5,6 +8,12 @@ namespace ConsoleUM
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
